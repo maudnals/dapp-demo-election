cdc5ade7becbfb909511ead7a31b89f0c8c25b89
Maud Nalpas <maud.nalpas@gmail.com>
2019-08-02 17:04:03 +0200

    feat(Election): added list of candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,8 +4,17 @@ contract Election {
     // read/write candidate
     string public candidate;
 
+    // declare data model for candidates
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    } 
+
     // constructor
     constructor() public {
         candidate = "Candidate 1";
     }
+
+    mapping(uint => Candidate) public candidates;
 }
